0c6faa4ec3b5c7e27d1b80281cd894a945b7ecfe
Hyunsoo <laugh4mile@naver.com>
2021-04-26 02:43:00 +0900

    feat: use API

--- App.js
+++ App.js
@@ -2,11 +2,23 @@ import React from 'react';
 import Loading from './Loading';
 import * as Location from 'expo-location';
 import { Alert } from 'react-native';
+import axios from 'axios';
+
+const API_KEY = '840959f06a781e455bd650e12dd664a2';
 
 export default class extends React.Component {
   state = {
     isLoading: true,
   };
+
+  getWeather = async (latitude, longitude) => {
+    console.log('data');
+    const { data } = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`
+    );
+    console.log(data);
+  };
+
   getLocation = async () => {
     try {
       const response = await Location.requestForegroundPermissionsAsync();
@@ -14,11 +26,11 @@ export default class extends React.Component {
       const {
         coords: { latitude, longitude },
       } = await Location.getCurrentPositionAsync();
+      this.getWeather(latitude, longitude);
       this.setState({ isLoading: false });
       // send to API and get weather
-      console.log('zzzzzzzzzz');
     } catch (error) {
-      Alert.alert('찾을 수 없다.', 'ㅠㅠ');
+      Alert.alert('에러', '찾을 수 없어요ㅠㅠ');
     }
   };
 
