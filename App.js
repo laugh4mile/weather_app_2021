3531ba2a79d31797eb95e37ebaf921bb8eb5b90c
Hyunsoo <laugh4mile@naver.com>
2021-04-25 22:02:34 +0900

    feat: learn flex

--- App.js
+++ App.js
@@ -5,7 +5,14 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.yellowView}>
+        {/* <Text style={styles.text}></Text> */}
+        <Text> ㅎㅇ</Text>
+      </View>
+      <View style={styles.blueView}>
+        {/* <Text style={styles.text}></Text> */}
+        <Text> ㅎㅇ</Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +21,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    // alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    color: 'black',
+  },
+  yellowView: {
+    flex: 1,
+    backgroundColor: 'yellow',
+  },
+  blueView: {
+    flex: 2,
+    backgroundColor: 'blue',
   },
 });
